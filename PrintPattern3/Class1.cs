eba66073af884797f0d47de06330e5226ab95e5a
Ayaz <Ayaz@DESKTOP-5QKM9AK>
2022-04-25 11:52:51 +0530

    Add project files.

---
+++ PrintPattern3/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrintPattern3
+{
+    internal class Class1
+    {
+        public static void pattern3()
+        {
+            
+            int starposition= 1;
+
+            while (starposition < 6)
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    
+                
+                        if (starposition == i)
+                        {
+                            Console.Write("*");
+                        }
+
+                        else
+                        {
+                            Console.Write(i);
+                        }
+                   
+                }
+                Console.WriteLine();
+                starposition++;
+            }
+           
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ PrintPattern3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrintPattern3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.pattern3();
+        }
+    }
+}
